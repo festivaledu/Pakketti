36c4e8578c7fd113853ece2d8f4ba3a08f30f1e0
SniperGER <janikschmidt@icloud.com>
2019-07-19 23:05:24 +0200

    Finalized Package Info layout

--- manager/src/components/PackageEditorPage.vue
+++ manager/src/components/PackageEditorPage.vue
@@ -13,112 +13,115 @@
 			</template>
 			
 			<template slot="items">
-				<div class="pivot-item" data-pivot-item="info" v-if="false">
-					<h4>Package Name</h4>
-					<p class="mb-3">
-						Give the package a descriptive name. This name will be reserved to you as long as you're using it for one of your packages.
-						<br>
-						The name of your package can be changed at any time to any currently unused name.
-					</p>
-					<input type="text" v-model="packageData.name" maxlength="50" autocomplete="off">
-					<p class="text-muted mb-0">{{ packageData.name.length }} / 50</p>
-					<a href="#" :disabled="packageData.name.length == 0">Check availability</a>
-					
-					<h4>Bundle Identifier</h4>
-					<p class="mb-3">
-						The bundle identifier is unique to your package and cannot be changed at a later point.
-						<br>
-						Bundle identifiers can be any unique string, but it is recommended to use reverse domain name notation (eg. com.example.package).
-					</p>
-					<input type="text" v-model="packageData.identifier" maxlength="50" autocomplete="off">
-					<p class="text-muted mb-0">{{ packageData.identifier.length }} / 50</p>
-					<a href="#" :disabled="!packageData.identifier.length">Check availability</a>
-					
-					<h4>Short Description</h4>
-					<p class="mb-3">
-						Add a short description to quickly describe the purpose and the use of your package.
-					</p>
-					<textarea v-model="packageData.shortDescription" maxlength="255" autocomplete="off" style="height: 158px" />
-					<p class="text-muted mb-0">{{ packageData.shortDescription.length }} / 255</p>
-				</div>
-				
 				<div class="pivot-item" data-pivot-item="info" v-if="true">
 					<div class="row">
 						<div class="col-12 col-md-6">
-							<h4>Package Name</h4>
-							<input type="text" placeholder="Example Package" v-model="packageData.name" maxlength="50" autocomplete="off">
-							<div class="row mb-4">
-								<div class="col-6 align-left">
-									<a href="#" :disabled="!packageData.name.length">Check availability</a>
-								</div>
-								<div class="col-6 align-right">
-									<p class="text-muted align-right mb-0">{{ packageData.name.length }} / 50</p>
+							<div class="mb-4">
+								<h4>Package Name</h4>
+								<input type="text" placeholder="Example Package" v-model="packageData.name" maxlength="50" autocomplete="off">
+								<div class="row">
+									<div class="col-6 align-left">
+										<a href="#" :disabled="!packageData.name.length">Check availability</a>
+									</div>
+									<div class="col-6 align-right">
+										<p class="text-muted align-right mb-0">{{ packageData.name.length }} / 50</p>
+									</div>
 								</div>
 							</div>
 
-							<h4>Bundle Identifier</h4>
-							<input type="text" placeholder="com.example.package" v-model="packageData.identifier" maxlength="50" autocomplete="off">
-							
-							<div class="row mb-4">
-								<div class="col-6 align-left">
-									<a href="#" :disabled="!packageData.identifier.length">Check availability</a>
-								</div>
-								<div class="col-6 align-right">
-									<p class="text-muted align-right mb-0">{{ packageData.identifier.length }} / 50</p>
+							<div class="mb-4">
+								<h4>Bundle Identifier</h4>
+								<input type="text" placeholder="com.example.package" v-model="packageData.identifier" maxlength="50" autocomplete="off">
+								
+								<div class="row">
+									<div class="col-6 align-left">
+										<a href="#" :disabled="!packageData.identifier.length">Check availability</a>
+									</div>
+									<div class="col-6 align-right">
+										<p class="text-muted align-right mb-0">{{ packageData.identifier.length }} / 50</p>
+									</div>
 								</div>
 							</div>
 						
-							<h4>Short Description</h4>
-							<textarea v-model="packageData.shortDescription" maxlength="255" autocomplete="off" style="height: 158px" />
-							<p class="text-muted align-right mb-4">{{ packageData.shortDescription.length }} / 255</p>
+							<div class="mb-4">
+								<h4>Short Description</h4>
+								<textarea v-model="packageData.shortDescription" maxlength="255" autocomplete="off" style="height: 158px" />
+								<p class="text-muted align-right mb-4">{{ packageData.shortDescription.length }} / 255</p>
+							</div>
 						
-							<h4>Description</h4>
-							<VueEditor :editorToolbar="editorToolbar" v-model="packageData.detailedDescription" />
-							
-							<div class="row">
-								<div class="col-6 align-left">
-									<!-- <a href="#">What's this?</a> -->
-								</div>
-								<div class="col-6 align-right">
-									<p class="text-muted align-right mb-0">{{ decodeText(packageData.detailedDescription).length }} / 3000</p>
+							<div class="mb-4">
+								<h4>Description</h4>
+								<VueEditor :editorToolbar="editorToolbar" v-model="packageData.detailedDescription" />
+								
+								<div class="row">
+									<div class="col-6 align-left">
+										<!-- <a href="#">What's this?</a> -->
+									</div>
+									<div class="col-6 align-right">
+										<p class="text-muted align-right mb-0">{{ decodeText(packageData.detailedDescription).length }} / 3000</p>
+									</div>
 								</div>
 							</div>
 						</div>
 						
 						<div class="col-12 col-md-6">
+							<div class="mb-4">
 							<h4>Device Families</h4>
-							<metro-checkbox>Phone</metro-checkbox>
-							<metro-checkbox>Tablet</metro-checkbox>
-							<metro-checkbox>Desktop</metro-checkbox>
-							<metro-checkbox>TV</metro-checkbox>
+								<div class="control-group">
+									<metro-checkbox>Phone</metro-checkbox>
+									<metro-checkbox>Tablet</metro-checkbox>
+									<metro-checkbox>Desktop</metro-checkbox>
+									<metro-checkbox>TV</metro-checkbox>
+								</div>
+							</div>
 							
-							<h4>Platform</h4>
-							<p>Select the platform your package will be available for.</p>
-							<metro-combo-box v-model="packageData.platform">
-								<select>
-									<option disabled selected>Select a platform</option>
-									<option>Windows</option>
-									<option>macOS</option>
-									<option>iOS (Cydia)</option>
-									<option>Linux (Debian)</option>
-									<option>Universal</option>
-								</select>
-							</metro-combo-box>
+							<div class="mb-4">
+								<h4>Platform</h4>
+								<p>Select the platform your package will be available for.</p>
+								<metro-combo-box v-model="packageData.platform">
+									<select>
+										<option disabled selected>Select a platform</option>
+										<option>Windows</option>
+										<option>macOS</option>
+										<option>iOS (Cydia)</option>
+										<option>Linux (Debian)</option>
+										<option>Universal</option>
+									</select>
+								</metro-combo-box>
+								
+								<p>Select the architecture your package was built for.</p>
+								<metro-combo-box v-model="packageData.architecture" :disabled="!packageData.platform">
+									<select>
+										<option disabled selected>Select an architecture</option>
+										<option>x86 32 bit</option>
+										<option>x86 64 bit</option>
+										<option>ARM</option>
+										<option>Universal</option>
+									</select>
+								</metro-combo-box>
+							</div>
 							
-							<p>Select the architecture your package was built for.</p>
-							<metro-combo-box class="mb-4" v-model="packageData.architecture" :disabled="!packageData.platform">
-								<select>
-									<option disabled selected>Select an architecture</option>
-									<option>x86 32 bit</option>
-									<option>x86 64 bit</option>
-									<option>ARM</option>
-									<option>Universal</option>
-								</select>
-							</metro-combo-box>
+							<div class="mb-4">
+								<h4>System Requirements</h4>
+								<input type="text" placeholder="Minimum required OS" v-model="packageData.minOSVersion" autocomplete="off">
+								<input type="text" placeholder="Maximum supported OS" v-model="packageData.maxOSVersion" autocomplete="off">
+							</div>
 							
-							<h4>System Requirements</h4>
-							<input type="text" placeholder="Minimum required OS" v-model="packageData.minOSVersion" autocomplete="off">
-							<input type="text" placeholder="Maximum supported OS" v-model="packageData.maxOSVersion" autocomplete="off">
+							<h4>Publishing Options</h4>
+							<div class="control-group">
+								<div class="radio">
+									<input type="radio" id="packageVisibilityOn" name="visible" :value="true" v-model="packageData.visible">
+									<label for="packageVisibilityOn">
+										<p class="item-label">Publish this package immediately</p>
+									</label>
+								</div>
+								<div class="radio">
+									<input type="radio" id="packageVisibilityOff" name="visible" :value="false" v-model="packageData.visible">
+									<label for="packageVisibilityOff">
+										<p class="item-label">Publish this package manually</p>
+									</label>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -152,8 +155,14 @@
 		margin-right: -12px;
 	}
 	
-	.input-wrapper {
-		display: table;
+	.control-group {
+		.checkbox, .radio {
+			display: flex;
+			
+			label {
+				flex: 1;
+			}
+		}
 	}
 	
 	.text-muted {
@@ -221,6 +230,12 @@
 				}
 			}
 			
+			h1, h2, h3, h4, h5, h6 {
+				& * {
+					font-size: inherit;
+				}
+			}
+			
 			pre {
 				font-family: "SF Mono", "Menlo", monospace;
 				font-size: 12px;
@@ -285,13 +300,13 @@ export default {
 				shortDescription: "",
 				detailedDescription: "",
 				platform: null,
-				architecture: null
+				architecture: null,
 				// releaseVersion: "1.0",
 				// releaseDescription: "Initial Release",
 				// bugsReportURL: null,
-				// minOSVersion: null,
-				// maxOSVersion: null,
-				// visible: true,
+				minOSVersion: null,
+				maxOSVersion: null,
+				visible: true,
 			}	
 		}
 	},
