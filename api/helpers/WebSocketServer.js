ac0ff6f7c41239639bc59f581d3a8b9345c40d03
SniperGER <janikschmidt@icloud.com>
2019-08-09 23:16:22 +0200

    Fixed WebSocket routes not matching parameters

--- api/helpers/WebSocketServer.js
+++ api/helpers/WebSocketServer.js
@@ -7,16 +7,20 @@ const authHelper = require("./AuthHelper");
 module.exports = (models, port = 62486) => {
 	let mappedMethods = {};
 	
-	const map = (path, layer) => {
+	const map = (path, regexp, keys, layer) => {
 		if (layer.route) {
-			layer.route.stack.forEach(map.bind(null, path.concat(split(layer.route.path))))
+			layer.route.stack.forEach(map.bind(null, path.concat(split(layer.route.path)), layer.regexp.toString().replace(/^\/\^(.+)\$\/i/, `^${path.join("\\/")}$1$`), layer.keys))
 		} else if (layer.name === 'router' && layer.handle.stack) {
-			layer.handle.stack.forEach(map.bind(null, path.concat(split(layer.regexp))))
+			layer.handle.stack.forEach(map.bind(null, path.concat(split(layer.regexp)), null, null))
 		} else if (layer.method) {
 			if (!mappedMethods[layer.method]) mappedMethods[layer.method] = {};
 			
 			const route = path.concat(split(layer.regexp)).filter(Boolean).join('/');
-			mappedMethods[layer.method]["/" + route] = layer.handle;
+			mappedMethods[layer.method]["/" + route] = {
+				handle: layer.handle,
+				regexp: regexp,
+				keys: keys
+			}
 		}
 	}
 	const split = (path) => {
@@ -35,7 +39,7 @@ module.exports = (models, port = 62486) => {
 		}
 	}
 	
-	controllers.stack.forEach(map.bind(null, []));
+	controllers.stack.forEach(map.bind(null, [], null, null));
 	
 	let socket = SocketIO(port);
 	
@@ -54,13 +58,31 @@ module.exports = (models, port = 62486) => {
 				});
 
 				if (mappedMethods[rq.method.toLowerCase()][rq.path]) {
-					await mappedMethods[rq.method.toLowerCase()][rq.path](rq, rs);
+					await mappedMethods[rq.method.toLowerCase()][rq.path].handle(rq, rs);
 				} else {
-					return ws.send(JSON.stringify({
-						_rqid: _data._rqid,
-						statusCode: 404,
-						body: `Cannot ${rq.method} /api/${rq.path}'`
-					}));
+					let route = Object.values(mappedMethods[rq.method.toLowerCase()]).find(_route => {
+						let match = rq.path.match(new RegExp(_route.regexp));
+						
+						if (match) {
+							if (_route.keys) {
+								Object.values(_route.keys).forEach((keyObj, index) => {
+									rq.params[keyObj.name] = match[index + 1]
+								});
+							}
+							
+							return _route;
+						}
+					});
+					
+					if (!route) {
+						return ws.send(JSON.stringify({
+							_rqid: _data._rqid,
+							statusCode: 404,
+							body: `Cannot ${rq.method} /api/${rq.path}'`
+						}));
+					}
+					
+					await route.handle(rq, rs)
 				}
 
 				ws.send(JSON.stringify(Object.assign(rs, {
