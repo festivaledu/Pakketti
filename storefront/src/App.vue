3397eecfc4c97992d67e0a5f51816c1bcbee8a4c
SniperGER <janikschmidt@icloud.com>
2019-09-10 18:09:23 +0200

    Fixed links not using accent color

--- storefront/src/App.vue
+++ storefront/src/App.vue
@@ -1,3 +1,22 @@
 <template>
 	<router-view />
 </template>
+
+<style lang="less">
+a {
+	color: var(--system-accent-color);
+	
+	&:hover:not(:active) {
+		color: var(--base-medium);
+	}
+	
+	&:active {
+		color: var(--base-medium-low);
+	}
+	
+	&[disabled] {
+		pointer-events: none;
+		color: var(--base-medium-low);
+	}
+}
+</style>
